4480d4c69684e68a5752dd6c1718354c88fb4ee1
Wen Zeng <47124806+zengwen1123@users.noreply.github.com>
2019-02-23 23:27:58 -0800

    Create sqaure-circles.js

---
+++ sqaure-circles.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+var x = []; // new empty array
+var y = []; // new empty array
+
+function draw() {
+  background(255);
+  noFill();
+
+  x.push(mouseX); // equivalent to append(x, mouseX)
+  y.push(mouseY); // equivalent to append(y, mouseY)
+ 
+  for (var i = 1; i < x.length; i = i + 10) {
+    rect(x[i+1], y[i], 2 + (x.length - i),1 + (x.length - i) );
+   
+	}
+
+  x = x.slice(-50); // keep the last 50 x values
+  y = y.slice(-50); // keep the last 50 y values
+}
